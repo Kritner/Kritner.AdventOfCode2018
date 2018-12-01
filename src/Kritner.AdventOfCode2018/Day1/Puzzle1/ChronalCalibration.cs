2578e155ce3ef473b1f86a1e385dfc363725f7d9
Russ Hammett <Kritner@gmail.com>
2018-12-01 16:53:15 -0500

    Day 1, Puzzle 1

---
+++ src/Kritner.AdventOfCode2018/Day1/Puzzle1/ChronalCalibration.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Kritner.AdventOfCode2018.Day1.Puzzle1
+{
+    /// <summary>
+    /// Chronal Calibration - see readme.
+    /// 
+    /// Sum some ints (can be positive or negative)
+    /// Parse string into useable ints
+    /// </summary>
+    public class ChronalCalibration
+    {
+        /// <summary>
+        /// Given an <see cref="IEnumerable{int}"/>, return the sum.
+        /// </summary>
+        /// <param name="values">The values to total</param>
+        /// <returns>int</returns>
+        public int CalibrateChronalsOrSomething(IEnumerable<int> values)
+        {
+            return values.Sum();
+        }
+
+        public IEnumerable<int> ParseInts(IEnumerable<string> stringsToParse)
+        {
+            List<int> numbers = new List<int>();
+
+            foreach (var s in stringsToParse)
+            {
+                if (int.TryParse(s, out var parsedInt))
+                {
+                    numbers.Add(parsedInt);
+                }
+            }
+
+            return numbers;
+        }
+
+        public IEnumerable<string> GetFileContents(string filePath)
+        {
+            List<string> lines = new List<string>();
+
+            using (var file = new StreamReader(filePath))
+            {
+                string line;
+                while((line = file.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            return lines;
+        }
+    }
+}
---
+++ test/Kritner.AdventOfCode2018.Tests/Day1/Puzzle1/ChronicalCalibrationTests.cs
@@ -0,0 +1,55 @@
+﻿using Kritner.AdventOfCode2018.Day1.Puzzle1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Kritner.AdventOfCode2018.Tests.Day1.Puzzle1
+{
+    public class ChronicalCalibrationTests
+    {
+        private ChronalCalibration _subject = new ChronalCalibration();
+
+        [Fact]
+        public void ShouldCalibrateCorrectly()
+        {
+            List<int> numbers = new List<int>()
+            {
+                1,
+                2, // 3
+                3, // 6
+                4, // 10
+                5, // 15
+                -50 // -35
+            };
+
+            var result = _subject.CalibrateChronalsOrSomething(numbers);
+
+            Assert.Equal(-35, result);
+        }
+
+        [Fact]
+        public void ShouldParseStringsIntoNumbersCorrectly()
+        {
+            List<string> stringsToParse = new List<string>();
+            stringsToParse.Add("+500"); // 500
+            stringsToParse.Add("-250"); // 250
+            stringsToParse.Add("1000"); // 1250
+
+            var resultingInts = _subject.ParseInts(stringsToParse);
+            var result = _subject.CalibrateChronalsOrSomething(resultingInts);
+
+            Assert.Equal(1250, result);
+        }
+
+        [Fact]
+        public void DoTheThing()
+        {
+            var file = _subject.GetFileContents("./Day1/Puzzle1/calibrationData.txt");
+            var ints = _subject.ParseInts(file);
+            var result = _subject.CalibrateChronalsOrSomething(ints);
+
+            Assert.Equal(522, result);
+        }
+    }
+}
