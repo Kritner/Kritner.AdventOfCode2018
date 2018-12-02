f59e159dbc2d04ddb183a5ab1a410285b8a0e917
Russ Hammett <Kritner@gmail.com>
2018-12-02 08:19:46 -0500

    Dya 2, puzzle 2

---
+++ src/Kritner.AdventOfCode2018/Day2/PrototypeFabricFinder.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Kritner.AdventOfCode2018.Day2
+{
+    public class PrototypeFabricFinder
+    {
+        public string GetProtoTypeFabric(IEnumerable<string> inventory)
+        {
+            var inventoryPermutations = SwapCharForEachInventoryPermutation(inventory);
+            var foundDuplicateish = inventoryPermutations
+                // Group by default
+                .GroupBy(gb => gb)
+                // We only want the instance where there's more than
+                // one in the group by (the prototype fabric)
+                .Where(w => w.Count() > 1)
+                .Select(s => new
+                {
+                    s.Key
+                })
+                .FirstOrDefault();
+            
+            // Return the prototype fabric, minus the "different" single character
+            return foundDuplicateish.Key.Replace("*", "");
+        }
+
+        /// <summary>
+        /// Builds and returns a <see cref="IEnumerable{string}"/>
+        /// containing every permutation of iventory items, with
+        /// one character (*) swapped in at a differing index.
+        /// 
+        /// I have no idea if that makes sense.
+        /// </summary>
+        /// <param name="inventory">each inventory id.</param>
+        /// <returns></returns>
+        private IEnumerable<string> SwapCharForEachInventoryPermutation(
+            IEnumerable<string> inventory
+        )
+        {
+            List<string> list = new List<string>();
+
+            foreach (var item in inventory)
+            {
+                // Create new strings and them to the list
+                // The new strings will be a copy of the original,
+                // with a single character (the index) swapped in with "*"
+                for (var index = 0; index < item.Length; index++)
+                {
+                    var charArrayOfItem = item.ToCharArray();
+                    charArrayOfItem[index] = '*';
+
+                    list.Add(new string(charArrayOfItem));
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ test/Kritner.AdventOfCode2018.Tests/Day2/PrototypeFabricFinderTests.cs
@@ -0,0 +1,57 @@
+﻿using Kritner.AdventOfCode2018.Common;
+using Kritner.AdventOfCode2018.Day2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Kritner.AdventOfCode2018.Tests.Day2
+{
+    public class PrototypeFabricFinderTests
+    {
+        private readonly PrototypeFabricFinder _subject =
+            new PrototypeFabricFinder();
+
+        public static IEnumerable<object[]> SampleData =>
+            new List<object[]>()
+            {
+                new object[]
+                {
+                    new[] {
+                        "abcde",
+                        "fghij",
+                        "klmno",
+                        "pqrst",
+                        "fguij",
+                        "axcye",
+                        "wvxyz"
+                    },
+                    // The IDs abcde and axcye are close, 
+                    // but they differ by two characters 
+                    // (the second and fourth). However, 
+                    // the IDs fghij and fguij differ by exactly 
+                    // one character, the third (h and u). 
+                    // Those must be the correct boxes.
+                    "fgij"
+                }
+            };
+
+        [Theory]
+        [MemberData(nameof(SampleData))]
+        public void ShouldPassSampleTest(string[] inventory, string expected)
+        {
+            var result = _subject.GetProtoTypeFabric(inventory);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void DoTheThing()
+        {
+            var file = Utilities.GetFileContents("./Day2/inventoryManagementData.txt");
+            var result = _subject.GetProtoTypeFabric(file);
+
+            Assert.Equal("asgwjcmzredihqoutcylvzinx", result);
+        }
+    }
+}
