d663ad297ccbe02ba45508c26a3b87c588c8ddf8
Russ Hammett <kritner@gmail.com>
2018-12-03 05:31:24 -0500

    Day 3, part 1, only part of it... gotta go to work :3

---
+++ src/Kritner.AdventOfCode2018/Day3/FabricSlicing.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Kritner.AdventOfCode2018.Day3
+{
+    public class FabricSlicing
+    {
+        protected Fabric GetFabricLayout(int width, int height, IEnumerable<string> fabricClaims)
+        {
+            var fabric = new Fabric()
+            {
+                Width = width,
+                Height = height,
+            };
+
+            fabric.FabricClaims = PopulateFabricClaims(fabricClaims);
+
+            return fabric;
+        }
+
+        protected IEnumerable<FabricSegments> PopulateFabricClaims(IEnumerable<string> fabricClaims)
+        {
+            List<FabricSegments> fabricSegments = new List<FabricSegments>();
+
+            // #1 @ 1,3: 4x4
+            Regex regex = new Regex(@"^#(?<id>\d*)\s@\s(?<x>\d*),(?<y>\d*):\s(?<width>\d*)x(?<height>\d*)$");
+
+            foreach (var claim in fabricClaims)
+            {
+                var matches = regex.Match(claim);
+
+                fabricSegments.Add(new FabricSegments()
+                {
+                    ClaimId = int.Parse(matches.Groups["id"].Value),
+                    Height = int.Parse(matches.Groups["height"].Value),
+                    Width = int.Parse(matches.Groups["width"].Value),
+                    StartCoordinateX = int.Parse(matches.Groups["x"].Value),
+                    StartCoordinateY = int.Parse(matches.Groups["y"].Value)
+                });
+            }
+
+            return fabricSegments;
+        }
+    }
+
+    public class Fabric
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public IEnumerable<FabricSegments> FabricClaims { get; set; }
+    }
+
+    public class FabricSegments
+    {
+        public int ClaimId { get; set; }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int StartCoordinateX { get; set; }
+        public int StartCoordinateY { get; set; }
+    }
+}
---
+++ test/Kritner.AdventOfCode2018.Tests/Day3/FabricSlicingTests.cs
@@ -0,0 +1,38 @@
+﻿using Kritner.AdventOfCode2018.Day3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Kritner.AdventOfCode2018.Tests.Day3
+{
+    public class FabricSlicingTests
+    {
+
+        private class PopulateFabricClaims_FabricSlicing : FabricSlicing
+        {
+            public IEnumerable<FabricSegments> GetFabricClaims(
+                IEnumerable<string> fabricClaims)
+            {
+                return PopulateFabricClaims(fabricClaims);
+            }
+        }
+
+        [Fact]
+        public void ShouldParseClaimsProperly()
+        {
+            var claim = "#1 @ 2,3: 4x5";
+
+            var subject = new PopulateFabricClaims_FabricSlicing();
+            var result = subject.GetFabricClaims(new[] { claim }).ToList();
+
+            Assert.True(result.Count == 1, nameof(result.Count));
+            Assert.True(result[0].ClaimId == 1, nameof(FabricSegments.ClaimId));
+            Assert.True(result[0].StartCoordinateX == 2, nameof(FabricSegments.StartCoordinateX));
+            Assert.True(result[0].StartCoordinateY == 3, nameof(FabricSegments.StartCoordinateY));
+            Assert.True(result[0].Width == 4, nameof(FabricSegments.Width));
+            Assert.True(result[0].Height == 5, nameof(FabricSegments.Height));
+        }
+    }
+}
